6b97d830f325a002312e442afb3761a1d7c28887
CamiloHernandezz <93105167+CamiloHernandezz@users.noreply.github.com>
2021-10-24 20:28:16 -0500

    Add files via upload

---
+++ RegresionLinal c++/Debug/CMakeFiles/RegresionLineal.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/camilo/Documentos/U.SegioArboleda/MachineLearning/RegresionLineal/RegresionLineal/exeigennorm.cpp" "/home/camilo/Documentos/U.SegioArboleda/MachineLearning/RegresionLineal/Debug/CMakeFiles/RegresionLineal.dir/exeigennorm.cpp.o"
+  "/home/camilo/Documentos/U.SegioArboleda/MachineLearning/RegresionLineal/RegresionLineal/linealregression.cpp" "/home/camilo/Documentos/U.SegioArboleda/MachineLearning/RegresionLineal/Debug/CMakeFiles/RegresionLineal.dir/linealregression.cpp.o"
+  "/home/camilo/Documentos/U.SegioArboleda/MachineLearning/RegresionLineal/RegresionLineal/main.cpp" "/home/camilo/Documentos/U.SegioArboleda/MachineLearning/RegresionLineal/Debug/CMakeFiles/RegresionLineal.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
